7924a45c26fe4f2a59b41a60100b19cca3ef6c25
BuildTools <unconfigured@null.spigotmc.org>
2020-07-13 12:24:41 +0200

    Add email sender

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const indexRouter = require('./routes/index')
 const apiRouter = require('./routes/api')
+const mailRouter = require('./routes/mail')
 const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3000
@@ -10,6 +11,7 @@ app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
     .use('/', indexRouter)
+    .use('/mail', mailRouter)
     .use('/api', apiRouter);
     
 app.listen(PORT, function() {
---
+++ routes/mail.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const nodemailer = require("nodemailer");
+require('dotenv').config()
+
+router.get('/', function(req, res, next) {
+    let transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+           user: process.env.EMAIL_USERNAME,
+           pass: process.env.EMAIL_PASSWORD
+        }
+    });
+
+    const message = {
+        from: 'info@minor-webdev.com', // Sender address
+        to: 'kylebot07@gmail.com',         // List of recipients (kunnen er meer zijn)
+        subject: 'Intakeformulier | HvA minor webdev', // Subject line
+        html: '<h1>Intakeformulier</h1><p>?code?</p>' // Bericht
+    };
+
+    transport.sendMail(message, function(err, info) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log(info);
+        }
+    });
+
+    res.redirect('/')
+  });
+
+module.exports = router;
