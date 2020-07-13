9b673aabb2467bac5e4965bf84f868cfcd235c2a
Giovanni Kaaijk <kaaijkg@gmail.com>
2020-07-13 12:18:49 +0200

    login

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const indexRouter = require('./routes/index')
 const apiRouter = require('./routes/api')
+const bodyParser = require("body-parser")
+const login = require('./routes/login')
 const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3000
@@ -9,10 +11,13 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
+    .use(express.json())
+    .use(express.urlencoded())  
     .use('/', indexRouter)
-    .use('/api', apiRouter);
+    .use('/api', apiRouter)
+    .use('/login', login)
+
     
 app.listen(PORT, function() {
     console.log(`Application started on port: ${PORT}`);
 });
-
---
+++ routes/login.js
@@ -0,0 +1,39 @@
+const express = require("express")
+require('dotenv').config()
+const app = express()
+const router = express.Router()
+const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.MONGO_URI
+const mongoDbClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+router.get('/', async (req, res) => {
+  res.render('login')
+})
+
+router.post('/', async (req, res) => {
+  console.log(req.body)
+    const userData = await login(req.body)
+    const password = req.body.password === userData.password ? true : false
+    if(password === true) {
+        console.log('correct')
+        res.redirect(`/admin/${userData.username}`)
+    } else {
+        console.log('incorrect')
+        res.redirect('/login')
+    }
+})
+
+async function login(data) {
+    try {
+        await mongoDbClient.connect()
+        const db = mongoDbClient.db('adminUsers')
+        let userData = await db.collection('user').findOne({username: data.username})
+        return userData
+      }
+      catch(e) {
+        console.error(e)
+      }
+}
+
+module.exports = router;
