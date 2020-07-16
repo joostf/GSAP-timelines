512b574012968e5aecbbf51f9661891cfaa66ef3
lennartdeknikker <me@lenn4rt.com>
2020-07-16 14:52:44 +0200

    add current maillist endpoint

--- public/js/new-user-form.js
+++ public/js/new-user-form.js
@@ -1,36 +1,31 @@
 const newUserForm = document.getElementById('new-users-fieldset')
 const inputElements = newUserForm.querySelectorAll('.entry')
+
 inputElements[inputElements.length - 1].addEventListener('change', createNewEntry)
 inputElements[inputElements.length - 1].addEventListener('keypress', createNewEntry)
 inputElements[inputElements.length - 1].addEventListener('paste', createNewEntry)
 
-
+// this function creates a new input element and label every time an emailadress is added.
 function createNewEntry(e) {
   const fieldset = document.getElementById('new-users-fieldset')
   const entries = fieldset.querySelectorAll('.entry')
+  const elementID = entries.length + 1
+  const newLabel = document.createElement('label')  
+  const newInput = document.createElement('input')
 
   this.addEventListener('change', removeEntryIfEmpty)
-
-  function removeEntryIfEmpty() {
-    if (this.querySelector('input').value === '') this.remove()
-  }
-
   this.removeEventListener('keypress', createNewEntry)
   this.removeEventListener('change', createNewEntry)
   this.removeEventListener('paste', createNewEntry)
 
-  const elementID = entries.length + 1
-
-  const newLabel = document.createElement('label')  
-  newLabel.classList.add('entry')
   newLabel.htmlFor = `emails[${elementID}]`
   newLabel.innerText = `emailadres student ${elementID}: `
+  newLabel.classList.add('entry')
 
   newLabel.addEventListener('change', createNewEntry)
   newLabel.addEventListener('keypress', createNewEntry)
   newLabel.addEventListener('paste', createNewEntry)
 
-  const newInput = document.createElement('input')
   newInput.type = 'email'
   newInput.id = `emails[${elementID}]`
   newInput.name = `emails[${elementID}]`
@@ -40,3 +35,7 @@ function createNewEntry(e) {
   newUserForm.appendChild(newLabel)
 }
 
+// this function removes empty input elements.
+function removeEntryIfEmpty() {
+  if (this.querySelector('input').value === '') this.remove()
+}
--- routes/api.js
+++ routes/api.js
@@ -1,84 +1,82 @@
 const { v4: uuidv4 } = require('uuid');
 const nodemailer = require("nodemailer");
+
 const express = require('express');
 const router = express.Router();
+
 const MongoClient = require('mongodb').MongoClient;
-const uri = process.env.MONGO_URI
-const mongoDbClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+const assert = require('assert')
+const url = process.env.MONGO_URI
+const dbName = 'users'
+const client = new MongoClient(url)
+
+router.get('/users', async function(req, res) {
+  const emails = req.body.emails  
+
+  client.connect(function(err) {
+    assert.equal(null, err)
+    console.log('connected succesfully')
+        
+    const db = client.db(dbName)
+      getAllUsers(db, function(docs) {
+        res.json(docs)
+        client.close()
+      })
+  })
+
+});
 
 router.post('/newuser', async function(req, res) {
-  console.log(req.body);
+  const emails = req.body.emails  
+
+  client.connect(function(err) {
+    assert.equal(null, err)
+    console.log('connected succesfully')
+        
+    const db = client.db(dbName)
+      getAllUsers(db, function() {
+        client.close()
+      })
+  })
+
   res.redirect('/admin')
-  
-    // const email = req.body.email    
-    // const id = uuidv4()
-    // // check for mailaddress in database. if it already exists, redirect
-    // const emailAlreadyUsed = await checkIfEmailIsUsed(email)
-    // console.log(emailAlreadyUsed)
-    
-    // if (emailAlreadyUsed) {
-    //   res.render('error', { message: 'emailadres is al gebruikt' })
-    // }
-    // else {
-    //   await createNewUserEntry(id, email)
-    //   sendMail(id, email)
-    //   res.redirect('/admin')
-    // }
-    
-    // else push new database entry with id and email.
+});
 
-    
-    async function checkIfEmailIsUsed(email) {
-      try {
-          await mongoDbClient.connect()
-          const db = mongoDbClient.db('users')
-          let userData = await db.collection('user').findOne({email: email})
-          return userData
-        }
-        catch(e) {
-          console.error(e)
-        }
-    }
-
-    async function createNewUserEntry(id, email) {
-      try {
-        await mongoDbClient.connect()
-        const db = mongoDbClient.db('users')
-        await db.collection('user').insertOne({
-          "userid" : id,
-          "email": email
-        })
-      }
-      catch(e) {
-        console.error(e)
-      }
-    }
-
-    function sendMail(id, email) {
-      let transport = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    });
-
-    const message = {
-        from: 'info@minor-webdev.com', // Sender address
-        to: email,         // List of recipients (kunnen er meer zijn)
-        subject: 'Intakeformulier | HvA minor webdev', // Subject line
-        html: `<h1>Intakeformulier</h1><p>${id}</p>` // Bericht
-    };
-
-    transport.sendMail(message, function(err, info) {
-        if (err) {
-          console.log(err)
-        } else {
-          console.log(info);
-        }
-    });
-    }
-
+const getAllUsers = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('user');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
   });
+}
+
+// function sendMail(id, email) {
+//   let transport = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: process.env.EMAIL_USERNAME,
+//         pass: process.env.EMAIL_PASSWORD
+//     }
+// });
+
+// const message = {
+//     from: 'info@minor-webdev.com', // Sender address
+//     to: email,         // List of recipients (kunnen er meer zijn)
+//     subject: 'Intakeformulier | HvA minor webdev', // Subject line
+//     html: `<h1>Intakeformulier</h1><p>${id}</p>` // Bericht
+// };
+
+// transport.sendMail(message, function(err, info) {
+//     if (err) {
+//       console.log(err)
+//     } else {
+//       console.log(info);
+//     }
+// });
+// }
 
 module.exports = router;
