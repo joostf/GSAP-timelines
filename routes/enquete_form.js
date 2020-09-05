7d2be0642bb9f7cffdfca72e5e7d95b0bf89d682
Giovanni Kaaijk <kaaijkg@gmail.com>
2020-09-05 13:47:29 +0200

    fix

--- routes/enquete_form.js
+++ routes/enquete_form.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+const MongoClient = require('mongodb').MongoClient;
+const url = process.env.MONGO_URI
+const dbName = 'users'
+
+
 router.get('/:id', function(req, res) {
     const id = req.params.id
     res.render('enquete_form', {
@@ -8,5 +13,16 @@ router.get('/:id', function(req, res) {
         id: id
     })
 })
+
+// router.post('/:id', async function(req,res) {
+//     const id = req.params.id
+//     MongoClient.connect(url, function(err, client) {
+//         console.log('connected succesfully')
+    
+//         const db = client.db(dbName)
+//         const user = await db.collection('user').findOne({hash: hash})
+//         client.close()
+//     })
+// })
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/:id', function(req, res) {
+router.get('/', function(req, res) {
     res.render('index', { title: 'Resultaten', id: req.params.id })
 })
 
