5fec398f96a3623c3871d1f293a4db81f117ac64
lennartdeknikker <me@lenn4rt.com>
2020-09-12 21:15:10 +0200

    add example image and progressive disclosure on input

--- public/js/challenge.js
+++ public/js/challenge.js
@@ -15,6 +15,8 @@ function setupFieldsets(buttons) {
   const allFieldsets = document.querySelectorAll('.context-challenge')
   const nextButtons = document.querySelectorAll('form .next')
   const previousButtons = document.querySelectorAll('form .previous')
+  const cssInput = document.getElementById('cssInput')
+  const jsInput = document.getElementById('jsInput')
 
   allFieldsets.forEach((fieldset, i) => {
       if(i !== 0) {
@@ -27,7 +29,10 @@ function setupFieldsets(buttons) {
       button.addEventListener('click', function(e) {
           e.preventDefault()
           allFieldsets[index].classList.add('hide')
+          console.log(index)          
           index += 1;
+          if (index === 1) cssInput.classList.remove('hide')
+          if (index === 2) jsInput.classList.remove('hide')
           allFieldsets[index].classList.remove('hide')
       })
   })
@@ -39,6 +44,8 @@ function setupFieldsets(buttons) {
               allFieldsets[index].classList.add('hide')
               index -= 1
               allFieldsets[index]?.classList?.remove('hide')
+              if (index === 0) cssInput.classList.add('hide')
+              if (index === 1) jsInput.classList.add('hide')
               if(allFieldsets[index] === undefined) {
                   allFieldsets[index + 1].classList.remove('hide')
                   index += 1
@@ -81,19 +88,37 @@ function compile() {
   compile();
 
   html.value= `
-  <main>
-  </main>`
+  <header>
+    <button>≡ Menu</button>
+    <h1>Coding Challenge</h1>
+  </header>`
   
     css.value= `
     body {
       font-family: "Exo", sans-serif;
       margin: 0;
+      background-color: white;
     }
 
-    main {
-      width:100vw;
-      height:100vh;
-      background: #fff;
+    header {
+      background-color: #ff9e00;
+      color: #001f53;
+      display: flex;
+      justify-content: space-between;
+      padding: 0 20px;
+    }
+
+    header h1 {
+      font-size: 1.5rem;
+    }
+
+    header button {
+      border: none;
+      color: #001f53;
+      font-weight: bold;
+      font-size: 1rem;
+      background-color: transparent;
+      cursor: pointer;
     }
     `
 js.value = `
