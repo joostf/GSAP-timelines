f1e20ce8a53b0c61eb914df4a049abdca2a5ea7a
lennartdeknikker <me@lenn4rt.com>
2020-09-27 15:17:01 +0200

    add fullscreen

--- public/js/challenge.js
+++ public/js/challenge.js
@@ -9,14 +9,36 @@ codePanels.forEach((panel) => {
 })
 
 const toggleElement = (e) => {
-  document.getElementById(e.target.dataset.for).classList.toggle('is-collapsed')
+  const rotatedIcons = document.querySelectorAll('.is-rotated')
+  rotatedIcons.forEach(icon => {
+    icon.classList.remove('is-rotated')
+  })
+
+  const openPanels = document.querySelectorAll('.is-opened')
+  openPanels.forEach(panel => {
+    panel.classList.remove('is-opened')
+  })
+
+  e.target.querySelector('i').classList.toggle('is-rotated')
+  document.getElementById(e.target.dataset.for).classList.toggle('is-opened')
 }
 
 const panelLabels = document.querySelectorAll('.panelLabel')
 panelLabels.forEach(label => {
-  label.addEventListener('click', (e) => toggleElement(e))
+  label.addEventListener('click', toggleElement)
 })
 
+const fullscreenButton = document.getElementById('button-fullscreen')
+fullscreenButton.addEventListener('click', e => toggleFullscreen(e)) 
+
+const toggleFullscreen = (e) => {
+  e.preventDefault()
+  fullscreenButton.classList.toggle('is-fullscreen')
+  const codeContainer = document.querySelector('.codepanels-container')
+  codeContainer.classList.toggle('is-fullscreen')
+  const codePanels = document.querySelectorAll('.codepanel')
+  codePanels.forEach(panel => panel.classList.toggle('is-fullscreen'))
+}
 // CodeFlask.onUpdate((code => {
 //   console.log(code)
 // }))
