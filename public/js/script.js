3e388a6e6664a4e5d7f4e580f1b73abecdc86301
Joost Faber <j.faber@hva.nl>
2021-01-19 20:46:48 +0100

    tweaking scene animation timing

--- public/js/script.js
+++ public/js/script.js
@@ -20,8 +20,8 @@ const tlSpaceWelder = () => {
   tl.to(spaceWelder, 1,  {opacity:1})
     .to(spaceWelder, 3, {x: floatingX, y: floatingY })
     .to(spaceWelder, 5, {scale:.5, x:'-5vw', y:'-12vh', transformOrigin: 'center center'}, '+=2')
-    .staggerTo(sparkles1,.1,{opacity: 1, x:-10, y:-10, repeat: -1, yoyo: false}, .025)
-    .staggerTo(sparkles2,.15, {opacity: 1, x:-10, y:-10, repeat: -1, yoyo: false, delay:0.5}, .025)
+    .staggerTo(sparkles1,.1,{opacity: 1, x:-10, y:-10, repeat: -1, yoyo: false, stagger:.025})
+    .staggerTo(sparkles2,.15, {opacity: 1, x:-10, y:-10, repeat: -1, yoyo: false, delay:0.5, stagger:.025})
   
   return tl
 }
@@ -35,7 +35,7 @@ const tlSpaceNerd = () => {
   tl.to(spaceNerd, 3, {opacity:1})
     .to(spaceNerd, 3, {x: floatingX, y: -floatingY})
     .to(spaceNerdArm,{rotate: 2, repeat: -1, duration: 2, yoyo: true })
-    .to(spaceNerd, 5, {scale:.5, y:'10vh', x:'40vw', transformOrigin: 'center center'}, '+=1')
+    .to(spaceNerd, 5, {scale:.5, y:'10vh', x:'40vw', transformOrigin: 'center center'}, '-=2')
   
     return tl
 }
@@ -54,7 +54,7 @@ const tlRocket = () => {
     .to(rocket, 2, {x:'5vw', y:'-5vh' })
     .to(rocket, 2, {x:'50vw', y:'-45vh', scale: .8, rotate: -100 })
     .to(rocket, 1, {y:'-30vh'}, '+=2')
-    .to(rocketFlames, 3, {opacity:0}, '-=1')
+    .to(rocketFlames, 3, {opacity:0}, '-=1.5')
     .to(rocketFlames, 100, {opacity:0}); // lame hack for now, handle rocketflames in own timeline so solve this
   
   return tl 
@@ -73,8 +73,8 @@ const tlPlanet = () => {
 // master timeline
 const master = () => {
   tlMaster.add(tlPlanet(),0.5)
-          .add(tlSpaceNerd(),1)
-          .add(tlSpaceWelder(),1.5)
+          .add(tlSpaceNerd(), 0.75)
+          .add(tlSpaceWelder(),1)
           .add(tlRocket(),2);
 }
 master();
