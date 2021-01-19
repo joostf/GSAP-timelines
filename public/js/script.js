19d695532e36ddcab8f5723b03ee0096f534999c
Joost Faber <j.faber@hva.nl>
2021-01-19 21:42:57 +0100

    added intro text (just an idea) & removed all trailing semicolons

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,21 @@
 const tlMaster = gsap.timeline({ defaults: {duration: 1 } })
 
 // elements
-const spaceNerd = $('#space-nerd');
+const spaceNerd = $('#space-nerd')
 const spaceNerdArm = $('#space-nerd-arm')
-const spaceWelder = $('#space-welder');
+const spaceWelder = $('#space-welder')
 const sparkles1 = $$('.spark-1')
 const sparkles2 = $$('.spark-2')
-const rocket = $('#rocket');
-const rocketFlames = $('#rocket-flames');
-const planet = $('#planet');
+const rocket = $('#rocket')
+const rocketFlames = $('#rocket-flames')
+const planet = $('#planet')
+const introText = $$('main > *')
 
 // child timelines
 // space welder
 const tlSpaceWelder = () => {
-  let floatingX =  Math.floor(Math.random() * 15) + 7; 
-  let floatingY = Math.floor(Math.random() * 10) + 5;
+  let floatingX =  Math.floor(Math.random() * 15) + 7
+  let floatingY = Math.floor(Math.random() * 10) + 5
   const tl = gsap.timeline({})
   
   tl.to(spaceWelder, 1,  {opacity:1})
@@ -28,8 +29,8 @@ const tlSpaceWelder = () => {
 
 // space nerd
 const tlSpaceNerd = () => {
-  let floatingX =  Math.floor(Math.random() * 5) + 3; 
-  let floatingY = Math.floor(Math.random() * 10) + 5;
+  let floatingX =  Math.floor(Math.random() * 5) + 3
+  let floatingY = Math.floor(Math.random() * 10) + 5
   const tl = gsap.timeline({})
   
   tl.to(spaceNerd, 3, {opacity:1})
@@ -43,19 +44,14 @@ const tlSpaceNerd = () => {
 // rocket
 const tlRocket = () => {
   const tl = gsap.timeline({})
-  const planetPosition = planet.getBoundingClientRect();
-  const rocketDestination = {
-    x: planetPosition.right - 270, // fix hardcoded offset
-    y: planetPosition.top
-  }
-  
+
   tl.to(rocket, 1, {opacity:1})
     .to(rocketFlames, .1, { opacity:0, repeat: -1, yoyo:true})
     .to(rocket, 2, {x:'5vw', y:'-5vh' })
     .to(rocket, 2, {x:'50vw', y:'-45vh', scale: .8, rotate: -100 })
     .to(rocket, 1, {y:'-30vh'}, '+=2')
     .to(rocketFlames, 3, {opacity:0}, '-=1.5')
-    .to(rocketFlames, 100, {opacity:0}); // lame hack for now, handle rocketflames in own timeline so solve this
+    .to(rocketFlames, 100, {opacity:0}) // lame hack for now, handle rocketflames in own timeline so solve this
   
   return tl 
 } 
@@ -68,22 +64,32 @@ const tlPlanet = () => {
     .to(planet, 10, {scale:6, x:'-5vh', transformOrigin: 'center top'})
   
   return tl 
-} 
+}
+
+// Introduction
+const tlIntro = () => {
+  const tl = gsap.timeline({})
+  
+  tl.staggerFrom(introText, 0.5,  {opacity:0, y:'-2.5vh', stagger:.75})
+    
+  return tl 
+}
 
 // master timeline
 const master = () => {
   tlMaster.add(tlPlanet(),0.5)
           .add(tlSpaceNerd(), 0.75)
           .add(tlSpaceWelder(),1)
-          .add(tlRocket(),2);
+          .add(tlRocket(),1)
+          .add(tlIntro(), 10)
 }
 master();
 
 // helper functions
 function $(element) {
-  return document.querySelector(element);
+  return document.querySelector(element)
 }
 
 function $$(elements) {
-  return document.querySelectorAll(elements);
+  return document.querySelectorAll(elements)
 }
