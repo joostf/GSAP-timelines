d9588396bc3c528502c92066d705813b7606e5b8
Joost Faber <j.faber@hva.nl>
2021-06-07 21:35:30 +0200

    chapter 1 & chapter 2 base setup, work in progress

--- static/js/chapter-one.js
+++ static/js/chapter-one.js
@@ -2,66 +2,70 @@ import { el } from './elements.js'
 
 const tlChapterOne = gsap.timeline({ defaults: {duration: 1 } })
 
-// child timelines
+/* child timelines */
 
 // Introduction
 const tlIntro = () => {
   const tl = gsap.timeline({})
   
   tl.from(el.introTitle, .5, {opacity:0})
-    .staggerFrom([el.introText, el.introCTA], .5, {opacity:0, y:'-2.5vh', delay:10, stagger:.75})
+    .staggerFrom([el.introText, el.introCTA], .25, {opacity:0, y:'-2.5vh', stagger:.25})
   return tl 
 }
-
-
 // rocket
 const tlRocket = () => {
   const tl = gsap.timeline({})
+  
+  tl.set(el.rocket, {xPercent:-20, yPercent:80, rotate:-20, transformOrigin:'50% 50%'})
+    .to(el.rocket, 1, {opacity:1, onComplete:flames(true)}, '+=3')
+    .to(el.rocket, 6, {xPercent:70, yPercent:45, scale:.1, rotate:-100, ease: 'elastic.out(.5, 0.3)'})
+    
+  return tl 
+} 
 
-  tl.to(el.rocket, 1, {opacity:1, onComplete:flames})
-    .to(el.rocket, 2, {x:'5vw', y:'-5vh' })
-    .to(el.rocket, 2, {x:'50vw', y:'-45vh', scale: .8, rotate: -100 })
-    .to(el.rocket, 1, {y:'-30vh', ease: 'elastic.out(1, 0.3)'}, '+=2')
-    .to(el.rocketFlames, 1, {opacity:0, display: 'none'}, '-=1.5')
+// rocket flames
+const tlRocketFlames = () => {
+  const tl = gsap.timeline({repeat: -1, yoyo:true})
+
+  tl.to(el.rocketFlames, .1, { opacity:0})
   
   return tl 
 } 
 
-// rocket
+// planet
 const tlPlanet = () => {
   const tl = gsap.timeline({})
   
-  tl.to(el.planet, 1,  {opacity:1})
-    .to(el.planet, 2, {attr:{viewBox:'0 0 200 300'}})
+  tl.set(el.planet, {xPercent:100, x:'120%', scale: 0.15,  transformOrigin:'50% 0'})
+    .to(el.planet, 1,  {opacity:1}, '+=2')
+    .to(el.planet, 3,  {xPercent:10, yPercent:50, scale: 1, ease: 'elastic.out(1, 0.3)'})
 
   return tl 
 }
 
-// master timeline
+/* main timeline */
 export const chapterOne = () => {
   tlChapterOne
           .pause()
           .add(tlIntro(), .25)
-          .add(tlPlanet(), .5)
-          .add(tlRocket(),1)
+          .add(tlPlanet(), .25)
+          .add(tlRocket(), .5)
 
   return tlChapterOne;
 }
 
-
-// Wrapping infinite tweens in callbacks preventing them from blocking the master timeline
-function coding (){
-  TweenMax.to(el.spaceNerdArm, 2, {rotate: 2, repeat: -1, yoyo: true })
-}
-
-function sparks() {
-  TweenMax.staggerTo(el.sparkles1,.1,{opacity: 1, x:-10, y:-10, repeat: -1, yoyo: false, stagger:.025})
-  TweenMax.staggerTo(el.sparkles2,.15, {opacity: 1, x:-10, y:-10, repeat: -1, yoyo: false, delay:.5, stagger:.025})
-}
+/* Helper functions */
 
 // Wrapping infinite tweens in callbacks preventing them blocking the master timeline
-function flames() {
-  TweenMax.to(el.rocketFlames, .1, { opacity:0, repeat: -1, yoyo:true})
+// This is not working???
+function flames(burning) {
+  return function() {
+    tlRocketFlames().paused(!burning);
+
+    if(!burning) {
+      tlRocketFlames().addPause(0);
+    }
+  }
 }
 
 
